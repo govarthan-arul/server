e397cbf80801303f3d64b2ef88eb4401a4a9d7ea
govarthanarul <govarthanarul@gmail.com>
2022-10-18 14:08:46 +0530

    port changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 5000
 const {MONGOURI} = require('./keys')
 //const multer = require('multer')
 //const fs = require('fs')
